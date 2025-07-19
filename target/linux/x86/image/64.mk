a9866b003ab8ebbb275b239983c657911302692b
Beomjun Kang <kals323@gmail.com>
2025-07-19 19:46:14 +0900

    x86: 64: add support for GMKtec G3 Plus
    Signed-off-by: Beomjun Kang <kals323@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -8,3 +8,14 @@ define Device/generic
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += generic
+
+define Device/gmktec_g3-plus
+  DEVICE_VENDOR := GMKtec
+  DEVICE_MODEL := G3 Plus
+  DEVICE_PACKAGES += \
+	kmod-dwmac-intel kmod-e1000e kmod-e1000 kmod-fs-vfat kmod-igb kmod-igc \
+  kmod-ixgbe kmod-r8169 kmod-drm-i915 kmod-iwlwifi kmod-rtw89-8852be \
+  openssh-server
+  GRUB2_VARIANT := generic
+endef
+TARGET_DEVICES += gmktec_g3-plus
