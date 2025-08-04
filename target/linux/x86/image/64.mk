e80d9b276e9c77aa047de881d412f822e5dd726d
Beomjun Kang <kals323@gmail.com>
2025-08-04 20:02:00 +0900

    add mitmproxy to device package
    Signed-off-by: Beomjun Kang <kals323@gmail.com>

--- target/linux/x86/image/64.mk
+++ target/linux/x86/image/64.mk
@@ -15,7 +15,7 @@ define Device/gmktec_g3-plus
   DEVICE_PACKAGES += \
 	kmod-dwmac-intel kmod-e1000e kmod-e1000 kmod-fs-vfat kmod-igb kmod-igc \
   kmod-ixgbe kmod-r8169 kmod-drm-i915 kmod-iwlwifi kmod-rtw89-8852be \
-  openssh-server
+  openssh-server mitmproxy
   GRUB2_VARIANT := generic
 endef
 TARGET_DEVICES += gmktec_g3-plus
